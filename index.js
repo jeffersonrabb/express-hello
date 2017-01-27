9436632cd9a6cedfc6b710667c21f9e70141dcff
Jefferson Rabb <jeff@atavist.net>
2017-01-27 12:57:34 -0500

    Just for fun.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res) {
-	res.send('Did I finally figure this out?????????')
+	res.send('At this point this is just for fun...')
 })
 
 app.listen(8080, function() {
