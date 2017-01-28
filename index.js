9c447e7a2eebc958d7da4902b19a3b2290824440
Jefferson Rabb <jeff@atavist.net>
2017-01-28 12:20:20 -0500

    Adding a DB container to try to hone in on the problem.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res) {
-	res.send('Exciting!!!!!!')
+	res.send('DB Running Too!')
 })
 
 app.listen(8080, function() {
